98bc7795e4d200d3b39e7056fec493df9275f62b
Andrew Carolan <andycarolan9@gmail.com>
2025-04-06 01:11:31 +0100

    Created JSON Helper class

---
+++ Utils/JSONHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace StatsTracker.Utils;
+
+public static class JSONHelper
+{
+    public static T? LoadFromJsonFile<T>(string filePath)
+    {
+        using (StreamReader sr = new StreamReader(filePath))
+        {
+            string jsonString = sr.ReadToEnd();
+            T? json = JsonSerializer.Deserialize<T>(jsonString);
+
+            if (json != null)
+            {
+                return json;
+            }
+        }
+        
+        return default;
+    }
+}
