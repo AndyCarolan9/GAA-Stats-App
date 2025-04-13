107c241e3e8a979b53f5ee013a844510b0cf6b7f
Andrew Carolan <andycarolan9@gmail.com>
2025-04-13 02:12:49 +0100

    Added all stats view and controller

---
+++ Controller/AllStatsController.cs
@@ -0,0 +1,27 @@
+﻿using StatsTracker.Model;
+using StatsTracker.Views;
+using StatsTracker.Views.Statistics;
+
+namespace StatsTracker.Controller;
+
+public class AllStatsController : IStatsController
+{
+    AllStatsView _view;
+    Match _match;
+
+    public AllStatsController(Match match)
+    {
+        _match = match;
+        _view = new AllStatsView();
+    }
+    
+    public IStatsView GetView()
+    {
+        return _view;
+    }
+
+    public DialogResult ShowDialog()
+    {
+        return _view.GetForm().ShowDialog();
+    }
+}
---
+++ Views/Statistics/AllStatsView.cs
@@ -0,0 +1,14 @@
+﻿namespace StatsTracker.Views.Statistics;
+
+public partial class AllStatsView : Form, IStatsView
+{
+    public AllStatsView()
+    {
+        InitializeComponent();
+    }
+
+    public Form GetForm()
+    {
+        return this;
+    }
+}
