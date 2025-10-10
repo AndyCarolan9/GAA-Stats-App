8bb2763677c1fa74cf6d2f4c759cec666102f52e
Andrew Carolan <andycarolan9@gmail.com>
2025-10-10 22:34:32 +0100

    Event Time Line view can no longer be resized

--- Views/Graphs/EventTimeLine.Designer.cs
+++ Views/Graphs/EventTimeLine.Designer.cs
@@ -31,51 +31,58 @@ partial class EventTimeLine
     /// </summary>
     private void InitializeComponent()
     {
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EventTimeLine));
-        HomeTeamLabel = new System.Windows.Forms.Label();
-        AwayTeamLabel = new System.Windows.Forms.Label();
-        EventPanel = new System.Windows.Forms.Panel();
+        ComponentResourceManager resources = new ComponentResourceManager(typeof(EventTimeLine));
+        HomeTeamLabel = new Label();
+        AwayTeamLabel = new Label();
+        EventPanel = new Panel();
         SuspendLayout();
         // 
         // HomeTeamLabel
         // 
-        HomeTeamLabel.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
-        HomeTeamLabel.Location = new System.Drawing.Point(8, 11);
+        HomeTeamLabel.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+        HomeTeamLabel.Location = new Point(7, 8);
         HomeTeamLabel.Name = "HomeTeamLabel";
-        HomeTeamLabel.Size = new System.Drawing.Size(218, 36);
+        HomeTeamLabel.Size = new Size(191, 27);
         HomeTeamLabel.TabIndex = 1;
         HomeTeamLabel.Text = "Home Team";
-        HomeTeamLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        HomeTeamLabel.TextAlign = ContentAlignment.MiddleCenter;
         // 
         // AwayTeamLabel
         // 
-        AwayTeamLabel.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
-        AwayTeamLabel.Location = new System.Drawing.Point(256, 11);
+        AwayTeamLabel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+        AwayTeamLabel.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+        AwayTeamLabel.Location = new Point(285, 8);
         AwayTeamLabel.Name = "AwayTeamLabel";
-        AwayTeamLabel.Size = new System.Drawing.Size(218, 36);
+        AwayTeamLabel.Size = new Size(191, 27);
         AwayTeamLabel.TabIndex = 2;
         AwayTeamLabel.Text = "Away Team";
-        AwayTeamLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        AwayTeamLabel.TextAlign = ContentAlignment.MiddleCenter;
         // 
         // EventPanel
         // 
         EventPanel.AutoScroll = true;
-        EventPanel.Location = new System.Drawing.Point(8, 50);
+        EventPanel.Location = new Point(7, 38);
+        EventPanel.Margin = new Padding(3, 2, 3, 2);
         EventPanel.Name = "EventPanel";
-        EventPanel.Size = new System.Drawing.Size(466, 797);
+        EventPanel.Size = new Size(477, 598);
         EventPanel.TabIndex = 0;
         // 
         // EventTimeLine
         // 
-        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        BackColor = System.Drawing.SystemColors.Control;
-        ClientSize = new System.Drawing.Size(481, 853);
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = SystemColors.Control;
+        ClientSize = new Size(488, 640);
         Controls.Add(AwayTeamLabel);
         Controls.Add(HomeTeamLabel);
         Controls.Add(EventPanel);
-        Icon = ((System.Drawing.Icon)resources.GetObject("$this.Icon"));
-        Location = new System.Drawing.Point(19, 19);
+        FormBorderStyle = FormBorderStyle.FixedSingle;
+        Icon = (Icon)resources.GetObject("$this.Icon");
+        Location = new Point(19, 19);
+        Margin = new Padding(3, 2, 3, 2);
+        MaximizeBox = false;
+        MinimizeBox = false;
+        Name = "EventTimeLine";
         Text = "Shot Event Timeline";
         ResumeLayout(false);
     }
